8fdef62fc392e7bdc62a03dffffb6b73c4d90a1c
admir <admirmabiki@gmail.com>
2024-02-09 23:02:27 +0100

    adding footer and header template with style css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./layouts/header/header.component";
+import { CategoryNavbarComponent } from "./layouts/category-navbar/category-navbar.component";
+import { FooterComponent } from "./layouts/footer/footer.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, HeaderComponent, CategoryNavbarComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'ang-blog-app';
