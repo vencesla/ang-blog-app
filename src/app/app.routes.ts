3b520a479c8d9a27cd907bf447595d24048c5a78
admir <admirmabiki@gmail.com>
2024-02-09 20:44:37 +0100

    init project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { SingleCategoryComponent } from './pages/single-category/single-category.component';
+import { SinglePostComponent } from './pages/single-post/single-post.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { TermsAndConditionComponent } from './pages/terms-and-condition/terms-and-condition.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'category', component: SingleCategoryComponent },
+    { path: 'post', component: SinglePostComponent },
+    { path: 'about', component: AboutUsComponent },
+    { path: 'term-conditions', component: TermsAndConditionComponent },
+    { path: 'contact', component: ContactUsComponent },
+];
